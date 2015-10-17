25dd9a677d3a8981c8d08b088d2c1e6c5de5a415
elTortuga <mds831@gmail.com>
2015-10-17 13:03:18 -0700

    Initial commit of an example using delegation in swift.

---
+++ delegationExample/delegationExample/ChildTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  ChildTableViewController.swift
+//  delegationExample
+//
+//  Created by Michael Simon on 10/17/15.
+//  Copyright © 2015 Michael Simon. All rights reserved.
+//
+
+import UIKit
+
+class ChildTableViewController: UITableViewController {
+  
+  let cities = ["El Centro", "San Francisco", "San Diego", "Cincinnati", "Albuquerque"]
+  weak var delegate : ParentProtocol?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return cities.count
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    
+    cell.textLabel?.text = cities[indexPath.row]
+    
+    return cell
+  }
+  
+  
+  override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    print("You selected city \(cities[indexPath.row])!")
+    callDelegate(cities[indexPath.row])
+  }
+  
+  func callDelegate(city:String) {
+    delegate?.method(city)
+  }
+  
+  /*
+  // Override to support conditional editing of the table view.
+  override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+  // Return false if you do not want the specified item to be editable.
+  return true
+  }
+  */
+  
+  /*
+  // Override to support editing the table view.
+  override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+  if editingStyle == .Delete {
+  // Delete the row from the data source
+  tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+  } else if editingStyle == .Insert {
+  // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+  }
+  }
+  */
+  
+  /*
+  // Override to support rearranging the table view.
+  override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+  
+  }
+  */
+  
+  /*
+  // Override to support conditional rearranging of the table view.
+  override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+  // Return false if you do not want the item to be re-orderable.
+  return true
+  }
+  */
+  
+  /*
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+  // Get the new view controller using segue.destinationViewController.
+  // Pass the selected object to the new view controller.
+  }
+  */
+  
+}
---
+++ delegationExample/delegationExample/ParentViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  delegationExample
+//
+//  Created by Michael Simon on 10/17/15.
+//  Copyright © 2015 Michael Simon. All rights reserved.
+//
+
+import UIKit
+
+protocol ParentProtocol : class {
+  func method(selectedString: String)
+}
+
+class ParentViewController: UIViewController {
+  
+  @IBOutlet var label: UILabel?
+  var childTableViewController = ChildTableViewController()
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    let childContainer = self.childViewControllers[0]
+    childTableViewController = childContainer.childViewControllers[0] as! ChildTableViewController
+    childTableViewController.delegate = self
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
+
+extension ParentViewController : ParentProtocol {
+  func method(selectedString: String) {
+    label?.text = selectedString
+  }
+}
+
